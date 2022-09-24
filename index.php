166825b111b57be3cc148dd58e86172066898ad0
tiac <71380895+Tiaccc@users.noreply.github.com>
2022-09-24 10:41:02 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicações informáticas</title>
+</head>
+<body>
+    <h1>Aplicações informáticas 22/23</h1>
+    
+   
+
+    <img src="image.png" alt="">
+
+    <a href="https://www.colegioalbergaria.pt/">Colégio de Albergaria</a>
+</body>
+
+<style>
+    img{
+        position: absolute;
+        right: 35%;
+    }
+    h1{
+        color: red;
+        text-align: center;
+        
+    }
+    a{
+    position: absolute;
+    top: 85%;
+    right: 50%;
+    text-decoration: none;
+    }
+</style>
+</html>
